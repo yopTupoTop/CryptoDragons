58cb150e710d9aa70417e719d3c5fea74ce7dc50
Tatiana <user@MacBook-Air-User.local>
2022-07-27 16:07:34 +0400

    add coin interface, contract for buttle

--- contracts/DragonCoin.sol
+++ contracts/DragonCoin.sol
@@ -1,14 +1,15 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IDragonCoin.sol";
 
-contract DragonCoin is ERC20 {
+contract DragonCoin is IDragonCoin, ERC20 {
     
     constructor () ERC20 ("Dragon Coin", "DNC") {
         
     }
 
-    function mint(address to, uint256 amount) external {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
